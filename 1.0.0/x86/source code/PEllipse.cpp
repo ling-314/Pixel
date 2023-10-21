6825ac75608edc9b797e1c5da2b1c5403e0c8155
ling-314 <2814026773@qq.com>
2023-10-21 13:46:10 +0800

    new file:   1.0.0/x64/Pixel.dll 	new file:   1.0.0/x64/Pixel.lib 	new file:   1.0.0/x64/source code/PConsole.cpp 	new file:   1.0.0/x64/source code/PConsole.h 	new file:   1.0.0/x64/source code/PConsoleOutput.cpp 	new file:   1.0.0/x64/source code/PEllipse.cpp 	new file:   1.0.0/x64/source code/PEvent.cpp 	new file:   1.0.0/x64/source code/PEvent.h 	new file:   1.0.0/x64/source code/PFunction.cpp 	new file:   1.0.0/x64/source code/PFunction.h 	new file:   1.0.0/x64/source code/PGraphic.cpp 	new file:   1.0.0/x64/source code/PLine.cpp 	new file:   1.0.0/x64/source code/PNode.cpp 	new file:   1.0.0/x64/source code/PNode.h 	new file:   1.0.0/x64/source code/PRectangle.cpp 	new file:   1.0.0/x64/source code/PRenderer.cpp 	new file:   1.0.0/x64/source code/PRenderer.h 	new file:   1.0.0/x64/source code/PScene.cpp 	new file:   1.0.0/x64/source code/PScene.h 	new file:   1.0.0/x64/source code/PSprite.cpp 	new file:   1.0.0/x64/source code/PText.cpp 	new file:   1.0.0/x64/source code/PType.cpp 	new file:   1.0.0/x64/source code/PType.h 	new file:   1.0.0/x64/source code/PWindow.cpp 	new file:   1.0.0/x64/source code/PWindow.h 	new file:   1.0.0/x86/Pixel.dll 	new file:   1.0.0/x86/Pixel.lib 	new file:   1.0.0/x86/source code/PConsole.cpp 	new file:   1.0.0/x86/source code/PConsole.h 	new file:   1.0.0/x86/source code/PConsoleOutput.cpp 	new file:   1.0.0/x86/source code/PEllipse.cpp 	new file:   1.0.0/x86/source code/PEvent.cpp 	new file:   1.0.0/x86/source code/PEvent.h 	new file:   1.0.0/x86/source code/PFunction.cpp 	new file:   1.0.0/x86/source code/PFunction.h 	new file:   1.0.0/x86/source code/PGraphic.cpp 	new file:   1.0.0/x86/source code/PLine.cpp 	new file:   1.0.0/x86/source code/PNode.cpp 	new file:   1.0.0/x86/source code/PNode.h 	new file:   1.0.0/x86/source code/PRectangle.cpp 	new file:   1.0.0/x86/source code/PRenderer.cpp 	new file:   1.0.0/x86/source code/PRenderer.h 	new file:   1.0.0/x86/source code/PScene.cpp 	new file:   1.0.0/x86/source code/PScene.h 	new file:   1.0.0/x86/source code/PSprite.cpp 	new file:   1.0.0/x86/source code/PText.cpp 	new file:   1.0.0/x86/source code/PType.cpp 	new file:   1.0.0/x86/source code/PType.h 	new file:   1.0.0/x86/source code/PWindow.cpp 	new file:   1.0.0/x86/source code/PWindow.h

---
+++ 1.0.0/x86/source code/PEllipse.cpp
@@ -0,0 +1,65 @@
+#include "PNode.h"
+
+Pixel::Ellipse::Ellipse(int x, int y, int radiusX, int radiusY) :
+	Graphic(x, y),
+	radiusX(radiusX),
+	radiusY(radiusY)
+{
+	size(Point(x - radiusX / 2, y - radiusY / 2), Point(x + radiusX / 2, y + radiusY / 2));
+	SetCollisionBox(Point(x - radiusX, y - radiusY), Point(x + radiusX, y + radiusY));
+}
+
+int Pixel::Ellipse::GetRadiusX() const
+{
+	return radiusX;
+}
+
+int Pixel::Ellipse::GetRadiusY() const
+{
+	return radiusY;
+}
+
+void Pixel::Ellipse::SetPosition(Point position)
+{
+	this->position(position);
+	size(Point(position.x - radiusX / 2, position.y - radiusY / 2), Point(position.x + radiusX / 2, position.y + radiusY / 2));
+	SetCollisionBox(size);
+}
+
+void Pixel::Ellipse::SetPosition(int x, int y)
+{
+	SetPosition(Point(x, y));
+}
+
+void Pixel::Ellipse::SetPositionX(int x)
+{
+	SetPosition(x, GetPositionY());
+}
+
+void Pixel::Ellipse::SetPositionY(int y)
+{
+	SetPosition(GetPositionX(), y);
+}
+
+void Pixel::Ellipse::SetRadiusX(int radiusX)
+{
+	this->radiusX = radiusX;
+	size(Point(position.x - radiusX / 2, position.y - radiusY / 2), Point(position.x + radiusX / 2, position.y + radiusY / 2));
+	SetCollisionBox(Point(position.x - radiusX, position.y - radiusY), Point(position.x + radiusX, position.y - radiusY));
+}
+
+void Pixel::Ellipse::SetRadiusY(int radiusY)
+{
+	this->radiusY = radiusY;
+	size(Point(position.x - radiusX / 2, position.y - radiusY / 2), Point(position.x + radiusX / 2, position.y + radiusY / 2));
+	SetCollisionBox(Point(position.x - radiusX, position.y - radiusY), Point(position.x + radiusX, position.y - radiusY));
+}
+
+void Pixel::Ellipse::Render(Renderer* renderer)
+{
+	renderer->RenderEllipse(position, scale, radiusX, radiusY, color, fill);
+	for (auto node : children)
+	{
+		node->Render(renderer);
+	}
+}
